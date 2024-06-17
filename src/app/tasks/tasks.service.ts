b5fa2f0b4c0f706815e5c64e8a59852b99d4a991
mate.knon <mate.knon@hunelco.com>
2024-06-17 16:58:52 +0200

    feat: finished demo project

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks')
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((t) => t.userId === userId);
   }
@@ -41,9 +49,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks()
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((t) => t.id !== taskId);
+    this.saveTasks()
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
   }
 }
